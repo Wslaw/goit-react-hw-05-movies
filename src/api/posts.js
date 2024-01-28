ff115c36a9fda34f2d6de6f7312891207e533a11
Wslaw <114644824+Wslaw@users.noreply.github.com>
2024-01-28 19:58:57 +0200

    updateApiFetch

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,46 +1,115 @@
-import axios from "axios";
 
 // const instance = axios.create({
-//     baseURL: "https://jsonplaceholder.typicode.com/posts",
+    //     baseURL: "https://jsonplaceholder.typicode.com/posts",
     
-// })
-
-// export const getAllPosts = ()=> {
-//     return instance.get("/");
-// }
-
-// export const searchPosts = (q, _page = 1) => {
-//     // return instance.get(`/?q=${q}&_limit=6&_page=${_page}`)
-//     return instance.get("/", {
-//         params: {
+    // })
+    
+    // export const getAllPosts = ()=> {
+        //     return instance.get("/");
+        // }
+        
+        // export const searchPosts = (q, _page = 1) => {
+            //     // return instance.get(`/?q=${q}&_limit=6&_page=${_page}`)
+            //     return instance.get("/", {
+                //         params: {
 //             q,
 //             _limit: 6,
 //             _page,
 //         }
 //     })
 // }
+// **********************************************************
+// import axios from "axios";
+// const API_KEY = 'a5e80a241b45048d42e42bcf67dfe2f1';
 
-const API_KEY = 'a5e80a241b45048d42e42bcf67dfe2f1';
+// const instance = axios.create({
+//       baseURL: 'https://api.themoviedb.org/3/',
+//     });
+    
+//     export const getAllPosts = async () => {
+//             try {
+//                     const response = await instance.get(
+//                           `trending/movie?language=en-US&api_key=${API_KEY}`
+//                         );
+//                         return response.data;
+                
+//                     } catch (error) {
+//                             throw error
+//                         }
+//                     };
+                    
+//                     export const searchMovies = async (query) => {
+//                             try {
+//                                     const response = await instance.get(`/search/search-movies?api_key=${API_KEY}`);
+//                                     return response.data;
+//                                 } catch (error) {
+//                                         throw error
+//     }
+// }
+// *******************************************************************
+// const API_KEY = 'a5e80a241b45048d42e42bcf67dfe2f1';
+// export const getAllMovies = require('node-fetch');
 
-const instance = axios.create({
-  baseURL: 'https://api.themoviedb.org/3?',
-});
+// const url = 'https://api.themoviedb.org/3/trending/all/day?language=en-US';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization:
+//       'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNWU4MGEyNDFiNDUwNDhkNDJlNDJiY2Y2N2RmZTJmMSIsInN1YiI6IjY1YjIzYWQ2MWM2MzI5MDE2YjkzNzEyOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2gd4mEIgWpUi3bRcFuw-y7GZqV-k0Ih9aL4xWWzG33o',
+//   },
+// };
 
-export const getAllPosts = async () => {
-    try {
-        const response = await instance.get(`/trending?api_key=${API_KEY}`)
-        return response.data;
+// getAllMovies(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error('error:' + err));
+// **********************************************************************
+const fetch = require('node-fetch');
 
-    } catch (error) {
-        throw error
-    }
+const url = 'https://api.themoviedb.org/3/trending/all/day?language=en-US';
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNWU4MGEyNDFiNDUwNDhkNDJlNDJiY2Y2N2RmZTJmMSIsInN1YiI6IjY1YjIzYWQ2MWM2MzI5MDE2YjkzNzEyOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2gd4mEIgWpUi3bRcFuw-y7GZqV-k0Ih9aL4xWWzG33o',
+
+  },
 };
 
-export const searchMovies = async (query) => {
-    try {
-        const response = await instance.get(`/search/search-movies?api_key=${API_KEY}`);
-        return response.data;
-    } catch (error) {
-        throw error
+export const getAllMovies = async () => {
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      throw new Error('Ошибка получения данных');
     }
-}
+    const data = await response.json();
+      return data;
+  } catch (error) {
+    console.error('Произошла ошибка:', error.message);
+    throw error;
+  }
+};
+// ********************************
+// let fetch;
+// if (typeof window === 'undefined') {
+//   // Если код выполняется в среде Node.js
+//   fetch = require('node-fetch');
+// } else {
+//   // Если код выполняется в браузере
+//   fetch = window.fetch;
+// }
+
+// const url = 'https://api.themoviedb.org/3/trending/all/day?language=en-US';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNWU4MGEyNDFiNDUwNDhkNDJlNDJiY2Y2N2RmZTJmMSIsInN1YiI6IjY1YjIzYWQ2MWM2MzI5MDE2YjkzNzEyOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2gd4mEIgWpUi3bRcFuw-y7GZqV-k0Ih9aL4xWWzG33o',
+//   },
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error('error:' + err));
--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,48 +1,101 @@
 import {useState, useEffect } from "react";
 
-import { getAllPosts } from "../../api/posts";
+import { getAllMovies } from "../../api/posts";
 
 import styles from "./posts.module.css";
 
-const Posts = () => {
-    const [posts, setPosts] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+// const Posts = () => {
+//     const [posts, setPosts] = useState([]);
+//     const [loading, setLoading] = useState(false);
+//     const [error, setError] = useState(null);
 
-    useEffect(()=> {        
-        const fetchPosts = async ()=> {
-            try {
-                setLoading(true);
-                const {data} = await getAllPosts();
-                setPosts(data?.length ? data : []);
-            }
-            catch(error) {
-                setError(error.message);
-            }
-            finally {
-                setLoading(false);
-            }
-        }
+//     useEffect(()=> {
+//         const fetchPosts = async ()=> {
+//             try {
+//                 setLoading(true);
+//                 const {data} = await getAllPosts();
+//                 setPosts(data?.length ? data : []);
+//             }
+//             catch(error) {
+//                 setError(error.message);
+//             }
+//             finally {
+//                 setLoading(false);
+//             }
+//         }
 
-        fetchPosts();
-    }, []);
+//         fetchPosts();
+//     }, []);
 
-    const elements = posts.map(({ id, title, body }) => (<li key={id} className={styles.item}>
-        <h3>{title}</h3>
-        <p>{body}</p>
-    </li>));
+//     const elements = posts.map(({ id, title, body }) => (<li key={id} className={styles.item}>
+//         <h3>{title}</h3>
+//         <p>{body}</p>
+//     </li>));
 
-    return (
-        <>
-            {error && <p className={styles.error}>{error}</p>}
-            {loading && <p>...Loading</p>}
-            {Boolean(elements.length) &&
-                (<ol className={styles.list}>
-                {elements}
-            </ol>)}
-        </>
+//     return (
+//         <>
+//             {error && <p className={styles.error}>{error}</p>}
+//             {loading && <p>...Loading</p>}
+//             {Boolean(elements.length) &&
+//                 (<ol className={styles.list}>
+//                 {elements}
+//             </ol>)}
+//         </>
+//     )
+// }
+
+
+// export default Posts;
+// **************************************************
+const Movies = () => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        setLoading(true);
+        const data = await getAllMovies();
+        setMovies(data?.results ? data.results : []);
+        console.log(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+  console.log(movies);
+  console.log('Number of movies:', movies.length); 
+
+  const elements = movies.map(
+    ({
+      id,title,
+      original_name,
+      original_language,
+      adult,
+      backdrop_path,
+      overview,
+    }) => (
+      <li key={id} className={styles.item}>
+        <h3 className={styles.title}>{original_name||title}</h3>
+        <p className={styles.text}>Original Language: {original_language}</p>
+        <p className={styles.text}>Adult: {adult ? 'Yes' : 'No'}</p>
+        <img className={styles.picture} src={backdrop_path} alt={overview} />
+      </li>
     )
-}
+  );
 
+  return (
+    <>
+      {error && <p className={styles.error}>{error}</p>}
+      {loading && <p>...Loading</p>}
+      {Boolean(elements.length) && <ol className={styles.list}>{elements}</ol>}
+    </>
+  );
+};
 
-export default Posts;
+export default Movies;
