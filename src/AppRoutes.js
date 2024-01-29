65662bae975b45e8d7014d4a4e56ef45e80e6be3
Wslaw <114644824+Wslaw@users.noreply.github.com>
2024-01-29 13:03:04 +0200

    updateSharedLayout

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,28 +1,29 @@
 import { Routes, Route } from 'react-router-dom';
-import MainMenu from 'components/MainMenu/MainMenu';
+// import MainMenu from 'components/MainMenu/MainMenu';
 import HomePage from 'Pages/HomePage/HomePage';
 import MoviesSearchPage from 'Pages/MoviesPage/MoviesPage';
 import NotFoundPage from 'Pages/NotFoundPage/NotFoundPage';
 import MovieDetailsPage from 'Pages/MovieDetailsPage/MovieDetailsPage';
 import CastPage from 'Pages/CastPage/CastPage';
-import ReviewsPage from './Pages/ReviewsPage/ReviewsPage'
-
+import ReviewsPage from './Pages/ReviewsPage/ReviewsPage';
+import SharedLayout from 'components/SharedLayout/SharedLayout';
 
 const AppRoutes = () => {
-    
-    return (
-      <>
-        <MainMenu />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/movies" element={<MoviesSearchPage />} />
-          <Route path="/movies/:id" element={<MovieDetailsPage />} />
-          <Route path="/movies/cast" element={<CastPage />} />
-          <Route path="/movies/reviews" element={<ReviewsPage />} />
+  return (
+    <>
+      {/* <MainMenu /> */}
+      <Routes>
+              <Route path="/" element={<SharedLayout />}>
+                 <Route index element={<HomePage />}/>
+          <Route path="movies" element={<MoviesSearchPage />} />
+          <Route path="movies/:id" element={<MovieDetailsPage />} />
+          <Route path="movies/cast" element={<CastPage />} />
+          <Route path="movies/reviews" element={<ReviewsPage />} />
           <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </>
-    );
-}
+        </Route>
+      </Routes>
+    </>
+  );
+};
 
 export default AppRoutes;
