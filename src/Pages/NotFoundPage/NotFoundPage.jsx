7dd58232a18c5a2709efdad73e4f7258dcf7c06e
Wslaw <114644824+Wslaw@users.noreply.github.com>
2024-01-30 21:30:34 +0200

    Update-4

--- src/Pages/NotFoundPage/NotFoundPage.jsx
+++ src/Pages/NotFoundPage/NotFoundPage.jsx
@@ -1,17 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import styles from './not-found-page.module.css';
 
 const NotFoundPage = () => {
-    return (
-      <div>
-        <h2 className={styles.title}>Cannot found this page</h2>
-        <button className={styles.btn}>
-          {' '}
-          <Link to="/">To Home page</Link>
-        </button>
-      </div>
-    );    
-
-}
+  return (
+    <div>
+      <h2 className={styles.title}>Cannot found this page.</h2>
+      <button className={styles.btn}>
+        {' '}
+        <Link className={styles.link} to="/">To Home page</Link>
+      </button>
+    </div>
+  );
+};
 
 export default NotFoundPage;
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,15 +1,17 @@
-import MainMenu from "components/MainMenu/MainMenu";
-import styles from './shared-layout.module.css'
-import { Outlet } from "react-router-dom";
+import MainMenu from 'components/MainMenu/MainMenu';
+import styles from './shared-layout.module.css';
+import { Outlet } from 'react-router-dom';
+import { Suspense } from 'react';
 
 const SharedLayout = () => {
-    
-    return (
-        <div className={styles.container}>
-            <MainMenu />
-            <Outlet />
-        </div>
-    )
-}
+  return (
+    <div className={styles.container}>
+      <MainMenu />
+      <Suspense fallback={<p>Wait...</p>}>
+        <Outlet />
+      </Suspense>
+    </div>
+  );
+};
 
 export default SharedLayout;
