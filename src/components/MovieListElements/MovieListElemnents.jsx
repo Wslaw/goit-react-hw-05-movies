c2b7eaf4847be0274d9c15a5c7723a9db735919e
Wslaw <114644824+Wslaw@users.noreply.github.com>
2024-02-01 01:20:15 +0200

    r

--- src/components/MovieListElements/MovieListElemnents.jsx
+++ src/components/MovieListElements/MovieListElemnents.jsx
@@ -31,7 +31,7 @@ const MovieListElements = ({ movies }) => {
 
   return (
     <div>
-      <ol className={styles.list}>{elements}</ol>
+      <ul className={styles.list}>{elements}</ul>
     </div>
   );
 };
