df0f85d8bc68bdcfaac11696e331edc4add72795
Wslaw <114644824+Wslaw@users.noreply.github.com>
2024-02-01 00:24:07 +0200

    Update MovieListElemnents.jsx

--- src/components/MovieListElements/MovieListElemnents.jsx
+++ src/components/MovieListElements/MovieListElemnents.jsx
@@ -3,24 +3,26 @@ import styles from './movie-list-elements.module.css';
 
 const BASE_URL = 'https://image.tmdb.org/t/p/w300';
 
+
 const MovieListElements = ({ movies }) => {
   const location = useLocation();
 
-  const handleItemClick = id => {
-    const url = `/movies/${id}`;
+// const handleItemClick = id => {
 
-    window.location.href = url;
-  };
+//   const url = `/movies/${id}`;
+  
+//   window.location.href = url;
+// };
+
 
   // console.log(movies);
   const elements = movies.map(({ id, title, original_name, poster_path }) => (
     <li key={id} className={styles.item}>
-      <div className={styles.itemWrap} onClick={() => handleItemClick(id)}>
+      <div className={styles.itemWrap} >
         <img
           className={styles.img}
-          src={poster_path ? BASE_URL + poster_path : ''}
-          alt={title}
-        />
+                   src={poster_path ? BASE_URL + poster_path : ''} alt={title} />
+
 
         <Link
           className={styles.title}
