6e0f7d5cb1256ba32aeae6fd9f9f398c83c3129c
Wslaw <114644824+Wslaw@users.noreply.github.com>
2024-02-01 00:38:00 +0200

    Update MovieListElemnents.jsx

--- src/components/MovieListElements/MovieListElemnents.jsx
+++ src/components/MovieListElements/MovieListElemnents.jsx
@@ -7,28 +7,22 @@ const BASE_URL = 'https://image.tmdb.org/t/p/w300';
 const MovieListElements = ({ movies }) => {
   const location = useLocation();
 
-// const handleItemClick = id => {
-
-//   const url = `/movies/${id}`;
-  
-//   window.location.href = url;
-// };
-
 
   // console.log(movies);
   const elements = movies.map(({ id, title, original_name, poster_path }) => (
     <li key={id} className={styles.item}>
-      <div className={styles.itemWrap} >
-        <img
-          className={styles.img}
-                   src={poster_path ? BASE_URL + poster_path : ''} alt={title} />
-
-
+      <div className={styles.itemWrap}>
         <Link
           className={styles.title}
           to={`/movies/${id}`}
           state={{ from: location }}
         >
+          <img
+            className={styles.img}
+            src={poster_path ? BASE_URL + poster_path : ''}
+            alt={title}
+          />
+
           {original_name || title}
         </Link>
       </div>
